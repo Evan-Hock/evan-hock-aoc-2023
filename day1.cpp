c4fb6af360b7c2263a937f349d0769ea97ddc205
Evan Hock <91491099+Evan-Hock@users.noreply.github.com>
2023-12-03 20:12:31 -0800

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <cstdint>
+
+inline int digit_to_int(char digit);
+
+int main() {
+    std::ifstream input{"input.txt"};
+    
+    std::uintmax_t out = 0;
+    
+    {
+        std::string line;
+        while (std::getline(input, line)) {
+            static const char* digits = "0123456789";
+            char first = line.find_first_of(digits),
+                 last = line.find_last_of(digits);
+                 
+            out += 10 * digit_to_int(line[first]) + digit_to_int(line[last]);
+        }
+    }
+    
+    std::cout << out << '\n';
+}
+
+int digit_to_int(char digit) {
+    return digit - '0';
+}
