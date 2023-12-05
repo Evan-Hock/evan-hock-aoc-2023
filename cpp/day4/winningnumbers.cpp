8a9b54f3f725676ac9d8c6a969cc24bf8fa83504
Evan Hock <91491099+Evan-Hock@users.noreply.github.com>
2023-12-04 21:30:14 -0800

    Create winningnumbers.cpp

---
+++ cpp/day4/winningnumbers.cpp
@@ -0,0 +1,51 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <set>
+
+std::set<int> get_lotto_numbers(std::istream& input);
+
+// day 4 part 1
+// sum the scores of lottery games based on this criteria
+// s = 2 ^ (n - 1)
+int main() {
+    std::ifstream input{"input.txt"};
+    
+    std::uintmax_t out;
+    
+    while (input.ignore(std::numeric_limits<std::streamsize>::max(), ':')) {
+        std::set<int> winning_numbers = get_lotto_numbers(input);
+        
+        input.ignore(std::numeric_limits<std::streamsize>::max(), '|');
+        
+        std::set<int> scratched_numbers = get_lotto_numbers(input);
+        
+        int score = 0;
+        for (int wn : winning_numbers) {
+            if (scratched_numbers.contains(wn)) {
+                score = score == 0 ? 1 : 2 * score;
+            }
+        }
+        
+        out += score;
+    }
+    
+    std::cout << out << std::endl;
+}
+
+std::set<int> get_lotto_numbers(std::istream& input) {
+    std::set<int> out;
+    do {
+        int x;
+        if (input >> x) {
+            out.insert(x);
+        }
+    } while (input);
+    
+    if (!input.eof()) {
+        input.clear();
+    }
+    
+    return out;
+}
