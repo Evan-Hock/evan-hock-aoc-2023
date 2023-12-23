d3955fdc3d58231ea6201cdb14302cfe2845293e
Evan-Hock <mocaointegral@gmail.com>
2023-12-23 01:25:30 -0800

    Fixing the solutions I have done to read from stdin

--- cpp/1/1/funnysum.cpp
+++ cpp/1/1/funnysum.cpp
@@ -7,13 +7,11 @@ inline int digit_to_int(char digit);
 // day 1 part 1
 // find the sum of the first and last digits on each line of the input file
 int main() {
-    std::ifstream input{"../../input/day1.txt"};
-    
     std::uintmax_t out = 0;
     
     {
         std::string line;
-        while (std::getline(input, line)) {
+        while (std::getline(std::cin, line)) {
             static const char* digits = "0123456789";
             std::size_t first = line.find_first_of(digits),
                         last = line.find_last_of(digits);
